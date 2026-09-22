b471ac7d8f0babc656c93a1c33b15281c96e4009
Julio César <julioprattes@gmail.com>
2026-09-22 16:35:03 -0300

    criando models e subindo para o banco de dados

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class BrandModel(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(
+        BrandModel,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    factory_year = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    model_year = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    value = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.model
