ec80f57970056e880bd863ebbd84d6a907861c76
Julio César <julioprattes@gmail.com>
2026-09-24 21:51:12 -0300

    Modificando templates e views

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from cars import views
 
+app_name = 'cars'
+
 urlpatterns = [
     path('cars/',views.cars_views, name='car')
 ]
--- cars/views.py
+++ cars/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from cars.models import CarModel
 
 def cars_views(request):
-    cars = CarModel.objects.all()
+    cars = CarModel.objects.all().order_by('model')
+    search = request.GET.get('search')
 
+    if search:
+        cars = CarModel.objects.filter(model__icontains=search).order_by('model')
+        
     context = {
         'cars':cars
     }
